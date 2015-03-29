5c1331f0b8e11cca5fc6fca221653773fb734a25
Geoffrey Seloana <geoff.seloana@gmail.com>
2015-03-29 02:11:11 +0200

    Added DTO's

---
+++ UniSchool/app/src/main/java/com/example/codetribe1/unischool/dto/StudentDTO.java
@@ -0,0 +1,73 @@
+package com.example.codetribe1.unischool.dto;
+
+/**
+ * Created by CodeTRibe1 on 2015-03-29.
+ */
+public class StudentDTO {
+    private Integer studentID;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int schoolID;
+    private int gradeID;
+
+    public StudentDTO() {
+    }
+
+    public StudentDTO(Integer studentID, String firstName, String middleName, String lastName, int schoolID, int gradeID) {
+        this.studentID = studentID;
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.schoolID = schoolID;
+        this.gradeID = gradeID;
+    }
+
+    public Integer getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(Integer studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSchoolID() {
+        return schoolID;
+    }
+
+    public void setSchoolID(int schoolID) {
+        this.schoolID = schoolID;
+    }
+
+    public int getGradeID() {
+        return gradeID;
+    }
+
+    public void setGradeID(int gradeID) {
+        this.gradeID = gradeID;
+    }
+}
