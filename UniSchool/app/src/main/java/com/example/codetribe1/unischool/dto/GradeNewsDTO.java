59195100203818deef0840feb8b62d9916787a53
Geoffrey Seloana <geoff.seloana@gmail.com>
2015-03-29 02:11:46 +0200

    Added DTO's

--- UniSchool/app/src/main/java/com/example/codetribe1/unischool/dto/GradeNewsDTO.java
+++ UniSchool/app/src/main/java/com/example/codetribe1/unischool/dto/GradeNewsDTO.java
@@ -4,4 +4,10 @@ package com.example.codetribe1.unischool.dto;
  * Created by CodeTRibe1 on 2015-03-29.
  */
 public class GradeNewsDTO {
+    private Integer studentID;
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private int schoolID;
+    private int gradeID;
 }
